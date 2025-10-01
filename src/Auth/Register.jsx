d774c43e767116970465d0be5a1559cc5e2cf939
arseniykv777 <senya_og@vk.com>
2025-10-01 19:50:31 +0500

    add registr validate

--- src/Auth/Register.jsx
+++ src/Auth/Register.jsx
@@ -1,46 +1,103 @@
 import { register } from "./api";
 import { Navigate } from "react-router-dom";
 import { useState } from "react";
+import "./css/register.css";
 
 function Register({ currentUser }) {
   if (currentUser) {
     return <Navigate to="/" replace />;
   }
 
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [loading, setLoading] = useState(false);
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+    passwordConfirm: "",
+  });
 
-  const handleEmailChange = (evt) => setEmail(evt.target.value);
-  const handlePassChange = (evt) => setPassword(evt.target.value);
+  const [errorFormData, setErrorFormData] = useState({
+    errorEmail: "",
+    errorPassword: "",
+    errorPasswordConfirm: "",
+  });
 
-  const clearFormData = () => (setEmail(""), setPassword(""));
+  const handleEmailChange = (evt) => setFormData({ ...formData, email: evt.target.value });
+  const handlePassChange = (evt) => setFormData({ ...formData, password: evt.target.value });
+  const handlePasswordConfirmChange = (evt) => setFormData({ ...formData, passwordConfirm: evt.target.value });
 
-  const handleFormSubmit = async (evt) => {
-    evt.preventDefault();
-    setLoading(true);
-    const result = await register(email, password);
-    setLoading(false);
+  const clearFormData = () => setFormData({ email: "", password: "", passwordConfirm: "" });
 
-    if (typeof result !== "object") {
-      console.log(result);
+  const validate = () => {
+    const errors = {
+      errorEmail: "",
+      errorPassword: "",
+      errorPasswordConfirm: "",
+    };
+
+    if (!formData.email) {
+      errors.errorEmail = "Адрес электронной почты не указан";
+    }
+    if (!formData.password) {
+      errors.errorPassword = "Пароль не указан";
+    }
+    if (!formData.passwordConfirm) {
+      errors.errorPasswordConfirm = "Повторный пароль не указан";
+    }
+    if (formData.password !== formData.passwordConfirm) {
+      errors.errorPassword = "Пароли не совпадают";
+      errors.errorPasswordConfirm = "Пароли не совпадают";
+    }
+
+    setErrorFormData(errors);
+    return !errors.errorEmail && !errors.errorPassword && !errors.errorPasswordConfirm;
+  };
+
+  const showErrorMessages = (code) => {
+    if (code === "auth/weak-password") {
+      setErrorFormData({
+        ...errorFormData,
+        errorPassword: "Слишком простой Пароль",
+        errorPasswordConfirm: "Слишком простой Пароль",
+      });
+    } else if (code === "auth/email-already-in-use") {
+      setErrorFormData({ ...errorFormData, errorEmail: "Эта почта уже используется" });
     }
   };
 
-  if (loading) return <p>Загрузка...</p>;
+  const handleFormSubmit = async (evt) => {
+    evt.preventDefault();
+    if (validate()) {
+      const result = await register(formData["email"], formData["password"]);
+      if (typeof result !== "object") {
+        showErrorMessages(result);
+      }
+    }
+  };
+
   return (
     <section>
       <h1>Регистрация</h1>
       <form onSubmit={handleFormSubmit} onReset={clearFormData}>
-        <label>
-          Адрес электронной почты
-          <input type="email" onChange={handleEmailChange} />
-        </label>
-        <label>
-          Пароль
-          <input type="password" onChange={handlePassChange} />
-        </label>
-
+        <div>
+          <label>
+            Адрес электронной почты
+            <input type="email" onChange={handleEmailChange} />
+          </label>
+          {errorFormData.errorEmail && <p className="errorText">{errorFormData.errorEmail}</p>}
+        </div>
+        <div>
+          <label>
+            Пароль
+            <input type="password" onChange={handlePassChange} />
+          </label>
+          {errorFormData.errorPassword && <p className="errorText">{errorFormData.errorPassword}</p>}
+        </div>
+        <div>
+          <label>
+            Повторите пароль
+            <input type="password" onChange={handlePasswordConfirmChange} />
+          </label>
+          {errorFormData.errorPasswordConfirm && <p className="errorText">{errorFormData.errorPasswordConfirm}</p>}
+        </div>
         <div className="buttons">
           <button type="reset">Сброс</button>
           <button type="submit">Зарегистрироваться</button>
